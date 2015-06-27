1c3326377ce09d9d673ca885b8af65b79fac814b
Shawn Chambless <shawn.chambless@gmail.com>
2015-06-27 09:56:46 -0600

    added basic function to add grain to db. not finished

--- public/app/controllers/databaseCtrl.js
+++ public/app/controllers/databaseCtrl.js
@@ -5,7 +5,8 @@ app.controller('databaseCtrl', function($scope, $http) {
     $scope.addGrainToDb = function(grain) {
         return $http({
             method: 'POST',
-            url: 'localhost:8081'
+            url: 'localhost:8081',
+            data: grain
         }, function(resp) {
             console.log(resp);
         })
