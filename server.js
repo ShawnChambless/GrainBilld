bf306c548a354ed34e12fccfb61f508ab2795a40
Shawn Chambless <shawn.chambless@gmail.com>
2015-06-30 15:41:52 -0400

    edited get and post functions(still not working

--- server.js
+++ server.js
@@ -3,24 +3,19 @@ var express = require('express'),
     mongoose = require('mongoose'),
     port = 8081,
     bodyParser = require('body-parser'),
+    databaseCtrl = require('./controllers/database/databaseCtrl'),
     cors = require('cors');
 
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(__dirname + '/public/app/views/databaseTmpl.html'));
 
-app.use(express.static(__dirname + '/public/app/views'));
-
-// app.get('/', function(req, res) {
-//     console.log('Got it')
-// });
-// app.post('/database', function(req, res) {
-//
-// });
-
+    app.get('/database', databaseCtrl.getGrain);
+    app.post('/database', databaseCtrl.addGrain);
 
 app.listen(port, function() {
     console.log('Listening on', port);
 });
-mongoose.connect('mongodb://localhost:27017/brewbetter', function(err, response) {
+mongoose.connect('mongodb://localhost:27017/brewabatch', function(err, response) {
     console.log(err, 'Mongo is also Listening');
 });
