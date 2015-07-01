7d80334c8c49f2063c366652067b01fd45d249a9
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-01 16:33:50 -0400

    add hops model

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ var express = require('express'),
     mongoose = require('mongoose'),
     port = 8081,
     bodyParser = require('body-parser'),
-    databaseCtrl = require('./controllers/database/databaseCtrl'),
+    grainCtrl = require('./controllers/database/grainCtrl'),
+    hopsCtrl = require('./controllers/database/hopsCtrl'),
     cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(__dirname + '/public/app/views/databaseTmpl.html'));
 
-    app.get('/database', databaseCtrl.getGrain);
-    app.post('/database', databaseCtrl.addGrain);
+    app.get('/database', grainCtrl.getGrain);
+    app.post('/database', grainCtrl.addGrain);
 
 app.listen(port, function() {
     console.log('Listening on', port);
