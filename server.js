0c443bc0ea515acd5de14509324437e8a5dd918d
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-04 15:46:34 -0600

    added endpoints for hops, fixed urls

--- server.js
+++ server.js
@@ -5,14 +5,21 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     grainCtrl = require('./controllers/database/grainCtrl'),
     hopsCtrl = require('./controllers/database/hopsCtrl'),
+    yeastCtrl = require('./controllers/database/yeastCtrl'),
     cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(__dirname + '/public/app/views/databaseTmpl.html'));
 
-    app.get('/database', grainCtrl.getGrain);
-    app.post('/database', grainCtrl.addGrain);
+    app.get('/database/ingredients/grain', grainCtrl.getGrain);
+    app.post('/database/ingredients/grain', grainCtrl.addGrain);
+
+    app.get('/database/ingredients/hops', hopsCtrl.getHops);
+    app.post('/database/ingredients/hops', hopsCtrl.addHops);
+
+    app.get('/database/ingredients/yeast', yeastCtrl.getYeast);
+    app.post('/database/ingredients/yeast', yeastCtrl.addYeast);
 
 app.listen(port, function() {
     console.log('Listening on', port);
