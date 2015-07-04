994fa77315ea790bf32cf341f8c4f0200ad8498b
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-04 15:44:39 -0600

    added a getGrain function, and set scope.grains to the result

--- public/app/controllers/databaseCtrl.js
+++ public/app/controllers/databaseCtrl.js
@@ -1,24 +1,35 @@
 var app = angular.module('personalProject');
 
-app.controller('databaseCtrl', function($scope, $http) {
+app.controller('databaseCtrl', function($scope, $http, $q) {
 
     $scope.addGrainToDb = function(grain) {
         return $http({
             method: 'POST',
-            url: 'http://localhost:8081/database',
+            url: 'http://localhost:8081/database/ingredients/grain',
             data: grain
-        }, function(resp) {
+        }).then(function(resp) {
             console.log(resp);
+            $scope.grain = '';
         });
     };
     $scope.addHopsToDb = function(hops) {
         return $http({
             method: 'POST',
-            url: 'http://localhost:8081/database',
-            data: grain
+            url: 'http://localhost:8081/database/ingredients/hops',
+            data: hops
         }, function(resp) {
             console.log(resp);
         });
     };
 
+    $scope.getGrain = function() {
+        var dfd = $q.defer();
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8081/database/ingredients/grain'
+        }).then(function(resp) {
+            dfd.resolve(resp);
+            $scope.grains = resp.data;
+        });
+    }();
 });
