9bcb388ce8e2558691ceaca22dcfbe3c165f4674
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-04 18:13:50 -0600

    Adding grain to database is working. Grain view is pretty ok.

--- public/app/controllers/databaseCtrl.js
+++ public/app/controllers/databaseCtrl.js
@@ -2,6 +2,58 @@ var app = angular.module('personalProject');
 
 app.controller('databaseCtrl', function($scope, $http, $q) {
 
+    $scope.showGrain = false;
+    $scope.showHops = false;
+    $scope.showYeast = false;
+    $scope.showGrainInDb = true;
+    $scope.showDescription = false;
+
+    $scope.toggleShowDescription = function() {
+        $scope.showDescription = !$scope.showDescription;
+    }
+
+    $scope.getGrain = function() {
+        var dfd = $q.defer();
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8081/database/ingredients/grain'
+        }).then(function(resp) {
+            dfd.resolve(resp);
+            $scope.grains = resp.data;
+        });
+    }();
+
+    $scope.toggleGrain = function() {
+        $scope.showGrain = !$scope.showGrain;
+        $scope.showHops = false;
+        $scope.showYeast = false;
+    };
+    $scope.toggleHops = function() {
+        $scope.showHops = !$scope.showHops;
+        $scope.showGrain = false;
+        $scope.showYeast = false;
+    };
+    $scope.toggleYeast = function() {
+        $scope.showYeast = !$scope.showYeast;
+        $scope.showGrain = false;
+        $scope.showHops = false;
+    };
+    $scope.toggleGrainInDb = function() {
+        $scope.showGrainInDb = !$scope.showGrainInDb;
+        $scope.showHopsInDb = false;
+        $scope.showYeastInDb = false;
+    };
+    $scope.toggleHopsInDb = function() {
+        $scope.showHopsInDb = !$scope.showHopsInDb;
+        $scope.showGrainInDb = false;
+        $scope.showYeastInDb = false;
+    };
+    $scope.toggleYeastInDb = function() {
+        $scope.showYeastInDb = !$scope.showYeastInDb;
+        $scope.showGrainInDb = false;
+        $scope.showHopsInDb = false;
+    }
+
     $scope.addGrainToDb = function(grain) {
         return $http({
             method: 'POST',
@@ -21,15 +73,4 @@ app.controller('databaseCtrl', function($scope, $http, $q) {
             console.log(resp);
         });
     };
-
-    $scope.getGrain = function() {
-        var dfd = $q.defer();
-        $http({
-            method: 'GET',
-            url: 'http://localhost:8081/database/ingredients/grain'
-        }).then(function(resp) {
-            dfd.resolve(resp);
-            $scope.grains = resp.data;
-        });
-    }();
 });
