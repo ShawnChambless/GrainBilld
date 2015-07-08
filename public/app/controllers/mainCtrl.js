0c2f1d7850e466c2552036af6f6e77bff8e817a0
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-08 16:58:39 -0600

    fixed fade toggle functions

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -15,28 +15,35 @@ app.controller('mainCtrl', function($scope, $location, $q, $http, mainService) {
     //jQuery functions
     $(function() {
 
-      $(".grainBox").on("click", function() {
-          $(".grainBox").toggleClass("open");
+      $('.grainBox').on('click', function() {
+          $('.grainBox .boxImage').toggleClass('open');
           $('input.grain').toggle(150);
-          $('.hopsBox .front, .yeastBox .front, .waterBox .front').fadeToggle(650);
+          $('.hopsBox, .yeastBox, .waterBox').fadeToggle(500);
+          $('.ingredient.hops, .ingredient.yeast, .ingredient.water').toggleClass('hidden');
+
       });
 
       $('.hopsBox').on('click', function() {
-          $('.hopsBox').toggleClass('open');
+          $('.hopsBox .boxImage').toggleClass('open');
           $('input.hops').toggle(150);
-          $('.grainBox .front, .yeastBox .front, .waterBox .front').fadeToggle(650);
+          $('.grainBox, .yeastBox, .waterBox').fadeToggle(500)
+          $('.ingredient.grain, .ingredient.yeast, .ingredient.water').toggleClass('hidden');
       });
 
       $('.yeastBox').on('click', function() {
-          $('.yeastBox').toggleClass('open');
+          $('.yeastBox .boxImage').toggleClass('open');
           $('input.yeast').toggle(150);
-          $('.hopsBox .front, .grainBox .front, .waterBox .front').fadeToggle(650);
+          $('.hopsBox, .grainBox, .waterBox').fadeToggle(500);
+          $('.ingredient.grain, .ingredient.hops, .ingredient.water').toggleClass('hidden');
+
       });
 
       $('.waterBox').on('click', function() {
-          $('.waterBox').toggleClass('open');
+          $('.waterBox .boxImage').toggleClass('open');
           $('input.water').toggle(150);
-          $('.hopsBox .front, .yeastBox .front, .grainBox .front').fadeToggle(650);
+          $('.hopsBox, .yeastBox, .grainBox').fadeToggle(500);
+          $('.ingredient.grain, .ingredient.yeast, .ingredient.hops').toggleClass('hidden');
+
       });
 
     });
