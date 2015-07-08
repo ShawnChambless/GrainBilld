12ca9264bc9a5cab57e32ab3c2c2e350774bb59b
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-07 22:03:00 -0600

    add angucomplete

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('personalProject', ['angular-loading-bar', 'ngRoute']);
+var app = angular.module('personalProject', ['angular-loading-bar', 'ngRoute', 'angucomplete-alt']);
 
 app.config(function($routeProvider) {
     $routeProvider
