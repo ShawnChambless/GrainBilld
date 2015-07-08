acf5867595b46e76406766c1f6f77cc4bc9c9681
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-07 22:04:22 -0600

    commented out grain search function until I can figure it out

--- public/app/services/mainService.js
+++ public/app/services/mainService.js
@@ -5,7 +5,7 @@ app.service('mainService', function($http, $q) {
     // this.grainSearch = function(grainSearchText) {
     //     return $http({
     //         method: 'GET',
-    //         url: 'http://localhost:8080/front/ingredients/grain?' + grainSearchText
+    //         url: 'http://localhost:8081/database/ingredients/grain/' + '?name=' + grainSearchText
     //     });
     // };
 
