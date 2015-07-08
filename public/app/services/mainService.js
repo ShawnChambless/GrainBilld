60fe337bc633f1b04f71031ba004368fcad4c96b
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-07 20:53:15 -0600

    add mainService

---
+++ public/app/services/mainService.js
@@ -0,0 +1,12 @@
+var app = angular.module('personalProject');
+
+app.service('mainService', function($http, $q) {
+
+    // this.grainSearch = function(grainSearchText) {
+    //     return $http({
+    //         method: 'GET',
+    //         url: 'http://localhost:8080/front/ingredients/grain?' + grainSearchText
+    //     });
+    // };
+
+});
