5a1f59542dcd249e23a1d61774dd91174c4ff2af
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-10 09:30:27 -0600

    cleaned up get grains function

--- public/app/services/mainService.js
+++ public/app/services/mainService.js
@@ -2,11 +2,11 @@ var app = angular.module('personalProject');
 
 app.service('mainService', function($http, $q) {
 
-    // this.grainSearch = function(grainSearchText) {
-    //     return $http({
-    //         method: 'GET',
-    //         url: 'http://localhost:8081/database/ingredients/grain/' + '?name=' + grainSearchText
-    //     });
-    // };
+    this.getGrainsInDb = function() {
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:8081/database/ingredients/grain/'
+        });
+    };
 
 });
