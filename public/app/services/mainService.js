2a94d4abb232c451c6bc5fabbf027336bb31397e
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-10 22:40:52 -0600

    revised getXInDb functions to accept a query param or not

--- public/app/services/mainService.js
+++ public/app/services/mainService.js
@@ -2,23 +2,51 @@ var app = angular.module('personalProject');
 
 app.service('mainService', function($http, $q) {
 
-    this.getGrainsInDb = function() {
-        return $http({
-            method: 'GET',
-            url: 'http://localhost:8081/database/ingredients/grain/'
-        });
+    this.getGrainsInDb = function(grainName) {
+        if (!grainName) {
+            return $http({
+                method: 'GET',
+                url: 'http://localhost:8081/database/ingredients/grain/'
+            });
+        }
+        else {
+            return $http({
+                method: 'GET',
+                url: 'http://localhost:8081/database/ingredients/grain/?name=' + grainName
+            });
+        }
     };
-    this.getHopsInDb = function() {
-        return $http({
-            method: 'GET',
-            url: 'http://localhost:8081/database/ingredients/hops/'
-        });
+
+    this.getHopsInDb = function(hopsName) {
+        if(!hopsName) {
+            return $http({
+                method: 'GET',
+                url: 'http://localhost:8081/database/ingredients/hops/'
+            });
+        }
+        else if (hopsName) {
+            return $http({
+                method: 'GET',
+                url: 'http://localhost:8081/database/ingredients/hops/?name=' + hopsName
+            });
+        }
+
     };
-    this.getYeastInDb = function() {
-        return $http({
+
+    this.getYeastInDb = function(yeastName) {
+        if(!yeastName) {
+                return $http({
+                method: 'GET',
+                url: 'http://localhost:8081/database/ingredients/yeast/'
+            });
+        }
+        else {
+            return $http({
             method: 'GET',
-            url: 'http://localhost:8081/database/ingredients/yeast/'
+            url: 'http://localhost:8081/database/ingredients/yeast/?name=' + yeastName
         });
+        }
+
     };
 
 });
