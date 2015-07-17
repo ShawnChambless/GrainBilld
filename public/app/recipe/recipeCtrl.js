8a094b23f95f615f4a9dd0373fafd1e6d14091ad
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-16 19:35:14 -0600

    working on it..

--- public/app/recipe/recipeCtrl.js
+++ public/app/recipe/recipeCtrl.js
@@ -1,9 +1,5 @@
 angular.module('personalProject')
 .controller('recipeCtrl', ['$scope', 'recipeService', function($scope, recipeService) {
 
-    $scope.saveRecipe = function() {
-        recipeService.saveRecipe();
-        console.log('pressed');
-    }
-
+    
 }]);
--- public/app/recipe/recipeService.js
+++ public/app/recipe/recipeService.js
@@ -1,16 +1,6 @@
 angular.module('personalProject')
-.service('recipeService', ['$http', '$firebaseArray', '$firebaseObject', 'loginService', 'mainService', function($http, $firebaseArray, $firebaseObject, loginService, mainService) {
-    this.authData = loginService.authObj.$getAuth();
-    this.ref = 'https://grainbilld.firebaseio.com';
-    this.firebaseRef = new Firebase(this.ref);
-    this.recipeRef = new Firebase(this.ref + '/' + this.authData.uid + '/recipes');
+.service('recipeService', ['$http', function($http) {
 
-    this.saveRecipe = function(newRecipe) {
-        newRecipe = {Grain: mainService.grainInRecipe, Hops: mainService.hopsInRecipe, Yeast: mainService.yeastInRecipe}
-        this.recipeRef.$firebaseObject(newRecipe).then(function(resp) {
-            console.log(resp);
-        });
-    }
 
 
 }]);
