927faaececa9be1c0c8c7fc345bbcbb2f123adde
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-19 17:39:29 -0600

    uncommented stuff, and cleaned up ng-shows

--- public/app/database/databaseCtrl.js
+++ public/app/database/databaseCtrl.js
@@ -1,18 +1,10 @@
 var app = angular.module('personalProject');
 
-app.controller('databaseCtrl',['$scope', '$http', '$q', 'mainService', function($scope, $http, $q, mainService) {
+app.controller('databaseCtrl',['$scope', '$http', '$q', 'mainService', 'grain', 'hops', 'yeast', function($scope, $http, $q, mainService, grain, hops, yeast) {
 
-    $scope.showGrain = false;
-    $scope.showHops = false;
-    $scope.showYeast = false;
-    $scope.showGrainInDb = true;
-    $scope.showHopsInDb = false;
-    $scope.showYeastInDb = false;
-    $scope.showDescription = false;
-
-    $scope.toggleShowDescription = function() {
-        $scope.showDescription = !$scope.showDescription;
-    };
+    $scope.grainsInDb = grain.data;
+    $scope.hopsInDb = hops.data;
+    $scope.yeastInDb = yeast.data;
 
     $scope.updateGrainSG = function(updateSG, itemId) {
         mainService.updateSG(updateSG, itemId).then(function(resp) {
@@ -21,105 +13,35 @@ app.controller('databaseCtrl',['$scope', '$http', '$q', 'mainService', function(
         });
     };
 
+    $scope.addGrainToDb = function(grain) {
+        return $http({
+            method: 'POST',
+            url: 'http://localhost:8081/database/ingredients/grain',
+            data: grain
+        }).then(function(resp) {
+            console.log('Added grain to DB', resp);
+            $scope.grain = '';
+        });
+    };
 
-    // Shows current list of grains in Database.
-    // $scope.getGrain = function() {
-    //     var dfd = $q.defer();
-    //     $http({
-    //         method: 'GET',
-    //         url: 'http://localhost:8081/database/ingredients/grain'
-    //     }).then(function(resp) {
-    //         console.log('Got Grain', resp);
-    //         dfd.resolve(resp);
-    //         $scope.grains = resp.data;
-    //     });
-    // }();
-    // // Shows current list of hops in Database.
-    // $scope.getHops = function() {
-    //     var dfd = $q.defer();
-    //     $http({
-    //         method: 'GET',
-    //         url: 'http://localhost:8081/database/ingredients/hops'
-    //     }).then(function(resp) {
-    //         console.log('Got Hops', resp);
-    //         dfd.resolve(resp);
-    //         $scope.hops = resp.data;
-    //     });
-    // }();
-    //
-    // $scope.getYeast = function() {
-    //     var dfd = $q.defer();
-    //     $http({
-    //         method: 'GET',
-    //         url: 'http://localhost:8081/database/ingredients/yeast'
-    //     }).then(function(resp) {
-    //         console.log('Got Yeast', resp)
-    //         dfd.resolve(resp);
-    //         $scope.yeast = resp.data;
-    //     });
-    // }();
-    //
-    // //Fairly self explanatory
-    // $scope.toggleGrain = function() {
-    //     $scope.showGrain = !$scope.showGrain;
-    //     $scope.showHops = false;
-    //     $scope.showYeast = false;
-    // };
-    // $scope.toggleHops = function() {
-    //     $scope.showHops = !$scope.showHops;
-    //     $scope.showGrain = false;
-    //     $scope.showYeast = false;
-    // };
-    // $scope.toggleYeast = function() {
-    //     $scope.showYeast = !$scope.showYeast;
-    //     $scope.showGrain = false;
-    //     $scope.showHops = false;
-    // };
-    // $scope.toggleGrainInDb = function() {
-    //     $scope.showGrainInDb = !$scope.showGrainInDb;
-    //     $scope.showHopsInDb = false;
-    //     $scope.showYeastInDb = false;
-    // };
-    // $scope.toggleHopsInDb = function() {
-    //     $scope.showHopsInDb = !$scope.showHopsInDb;
-    //     $scope.showGrainInDb = false;
-    //     $scope.showYeastInDb = false;
-    // };
-    // $scope.toggleYeastInDb = function() {
-    //     $scope.showYeastInDb = !$scope.showYeastInDb;
-    //     $scope.showGrainInDb = false;
-    //     $scope.showHopsInDb = false;
-    // }
-    //
-    // $scope.addGrainToDb = function(grain) {
-    //     return $http({
-    //         method: 'POST',
-    //         url: 'http://localhost:8081/database/ingredients/grain',
-    //         data: grain
-    //     }).then(function(resp) {
-    //         console.log('Added grain to DB', resp);
-    //         $scope.grain = '';
-    //     });
-    // };
-    //
-    // $scope.addHopsToDb = function(hops) {
-    //     return $http({
-    //         method: 'POST',
-    //         url: 'http://localhost:8081/database/ingredients/hops',
-    //         data: hops
-    //     }).then(function(resp) {
-    //         console.log('Added hops to DB', resp);
-    //         $scope.addHops = '';
-    //     });
-    // };
-    // $scope.addYeastToDb = function(yeast) {
-    //     return $http({
-    //         method: 'POST',
-    //         url: 'http://localhost:8081/database/ingredients/yeast',
-    //         data: yeast
-    //     }).then(function(resp) {
-    //         console.log('Added yeast to DB', resp, resp.data);
-    //         $scope.addYeast = '';
-    //     })
-    // }
+    $scope.addHopsToDb = function(hops) {
+        return $http({
+            method: 'POST',
+            url: 'http://localhost:8081/database/ingredients/hops',
+            data: hops
+        }).then(function(resp) {
+            console.log('Added hops to DB', resp);
+            $scope.addHops = '';
+        });
+    };
+    $scope.addYeastToDb = function(yeast) {
+        return $http({
+            method: 'POST',
+            url: 'http://localhost:8081/database/ingredients/yeast',
+            data: yeast
+        }).then(function(resp) {
+            console.log('Added yeast to DB', resp, resp.data);
+            $scope.addYeast = '';
+        })
+    }
 }]);
