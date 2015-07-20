6ea689aee8bda3e11eb70777e434ec3e413dfe41
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-20 00:29:27 -0600

    add indexCtrl for routes

---
+++ public/app/index/indexCtrl.js
@@ -0,0 +1,8 @@
+angular.module('personalProject')
+.controller('indexCtrl', ['$scope', '$location', function($scope, $location) {
+
+    $scope.showNewBatch = false;
+    $scope.showIngredientInfo = true;
+    $scope.showMyRecipes = true
+
+}]);
---
+++ public/app/min/indexCtrl-min.js
@@ -0,0 +1,1 @@
+angular.module("personalProject").controller("indexCtrl",["$scope","$location",function(o,e){o.showNewBatch=!1,o.showIngredientInfo=!0,o.showMyRecipes=!0}]);
