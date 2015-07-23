f24c50001008ea27cf74e3d89b1ddf4ace560155
Shawn Chambless <shawn.chambless@gmail.com>
2015-07-22 20:58:49 -0600

    Auth working

--- public/app/login/loginCtrl.js
+++ public/app/login/loginCtrl.js
@@ -2,15 +2,18 @@ var app = angular.module('personalProject')
     .controller('loginCtrl', ['$scope', '$http', 'loginService', function($scope, $http, loginService) {
 
         $scope.authData = loginService.authData;
-        $scope.user = '';
-        $scope.newUser = '';
+
 
         $scope.login = function(email, password) {
             loginService.login($scope.user.email, $scope.user.password)
+            $scope.user = '';
+            $scope.newUser = '';
         }
 
         $scope.register = function(email, password) {
             loginService.register($scope.newUser.email, $scope.newUser.password)
+            $scope.user = '';
+            $scope.newUser = '';
         }
 
 
