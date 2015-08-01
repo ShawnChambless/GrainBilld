cf22f41ed5be6fc779028a9bde53a4fd8a2af9a8
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-01 17:39:07 -0600

    edited file references

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-var express = require('express'),
+var express =       require('express'),
     app = express(),
-    session = require('express-session'),
-    mongoose = require('mongoose'),
-    // passport = require('passport'),
-    // flash = require('connect-flash'),
-    UserCtrl = require('./controllers/users/userCtrl'),
+    session =       require('express-session'),
+    mongoose =      require('mongoose'),
+    passport =      require('passport'),
+    UserCtrl =      require('./api/controllers/users/userCtrl'),
     port = 8081,
-    bodyParser = require('body-parser'),
-    grainCtrl = require('./controllers/database/grainCtrl'),
-    hopsCtrl = require('./controllers/database/hopsCtrl'),
-    yeastCtrl = require('./controllers/database/yeastCtrl'),
-    cors = require('cors');
+    bodyParser =    require('body-parser'),
+    grainCtrl =     require('./api/controllers/database/grainCtrl'),
+    hopsCtrl =      require('./api/controllers/database/hopsCtrl'),
+    yeastCtrl =     require('./api/controllers/database/yeastCtrl'),
+    cors =          require('cors');
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(__dirname + '/public/app/views'));
+app.use(express.static(__dirname + '/public'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+    app.post('/auth/local/signup', passport.authenticate( 'local-signup' ), function(req, res){
+      res.json(req.user);
+    });
+    app.post('/auth/local/login', passport.authenticate( 'local-login' ), function(req, res){
+      res.json(req.user);
+    });
+    app.get('/auth/logout', function(req, res){
+      req.logout();
+      res.redirect('/');
+      return res.send('logged out');
+    });
+
+    // app.post(   '/api/users',          userCtrl.create );
+    // app.get(    '/api/users/:user_id', userCtrl.retrieveOne );
+    // app.get(    '/api/user/',          userCtrl.getCurrentUser);
+    // app.put(    '/api/users/:user_id', userCtrl.update );
+    // app.delete( '/api/users/:user_id', userCtrl.remove );
+
+
 
     app.get('/database/ingredients/grain', grainCtrl.getGrain);
     app.post('/database/ingredients/grain', grainCtrl.addGrain);
