2a1bdecf6ecc92f821dcc4d486e836790b3d688e
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-01 18:43:10 -0600

    IT WORKS!

--- public/app/login/loginCtrl.js
+++ public/app/login/loginCtrl.js
@@ -1,5 +1,12 @@
 var app = angular.module('personalProject')
-    .controller('loginCtrl', ['$scope', '$http', 'loginService', function($scope, $http, loginService) {
+.controller('loginCtrl', ['$scope', '$http', 'loginService', function($scope, $http, loginService) {
 
+    $scope.register = function(firstName, lastName, email, password) {
+        loginService.register(firstName, lastName, email, password);
+    };
 
-    }]);
+    $scope.login = function(email, password) {
+        loginService.login(email, password);
+    };
+
+}]);
--- public/app/login/loginService.js
+++ public/app/login/loginService.js
@@ -1,5 +1,45 @@
 angular.module('personalProject')
-.service('loginService', ['$location', function($location) {
+.service('loginService', ['$location', '$http', function($location, $http) {
 
-    
+    this.register = function(firstName, lastName, email, password) {
+        return $http({
+            method: 'POST',
+            url:    '/auth/local/signup',
+            data: {
+                firstName:  firstName,
+                lastName:   lastName,
+                email:      email,
+                password:   password
+            }
+        }).then(function(resp) {
+            this.login(email, password);
+            console.log(resp.data);
+            return resp.data;
+        }.bind(this));
+    };
+
+    this.login = function(email, password) {
+        return $http({
+            method: 'POST',
+            url: '/auth/local/login',
+            data: {
+                email:      email,
+                password:   password
+            }
+        }).then(function(resp) {
+            console.log(resp.data);
+            return resp.data;
+        });
+    };
+
+    this.getCurrentUser = function() {
+        return $http({
+            method: 'GET',
+            url:    '/api/user'
+        }).then(function(resp) {
+            console.log(resp.data);
+            return resp.data;
+        });
+    };
+
 }]);
