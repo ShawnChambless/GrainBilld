f4d249e2e88bdda150f532186bbe6becee189c78
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-01 18:43:21 -0600

    production files

--- public/production/minifiedJS/login/loginCtrl.js
+++ public/production/minifiedJS/login/loginCtrl.js
@@ -1,1 +1,1 @@
-var app=angular.module("personalProject").controller("loginCtrl",["$scope","$http","loginService",function(o,l,r){}]);
+var app=angular.module("personalProject").controller("loginCtrl",["$scope","$http","loginService",function(n,o,r){n.register=function(n,o,e,i){r.register(n,o,e,i)},n.login=function(n,o){r.login(n,o)}}]);
