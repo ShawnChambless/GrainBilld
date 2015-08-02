7cb2807c5e61984c302a329fd98eeaf9a2027b67
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-01 18:42:41 -0600

    added retrieveOne back to controller

--- api/controllers/users/userCtrl.js
+++ api/controllers/users/userCtrl.js
@@ -28,6 +28,35 @@ module.exports = {
     return def.promise;
   } ,
 
+  retrieveOne: function(req, res){
+    var def = $q.defer();
+    var query = {};
+    if (req.user || req.params.user_id) query = { "_id": req.params.user_id };
+    else query = { "email": req.body.email };
+    User.findOne(query)
+    .exec().then(function(user, err){
+      if (err) {
+        console.log(err);
+        if (req.qpromise) {def.reject(err);}
+        else {return res.status(500).json(err);}
+      }
+      else if (user) {
+        if (query._id) return res.status(200).json(user);
+        if (checkHash(req.body.password, user.password)) {
+          if (req.qpromise) {def.resolve(user);}
+          else {return res.status(200).json(user);}
+        }
+        if (!checkHash(req.body.password, user.password)) {
+          if (req.qpromise) {def.reject('Invalid password');}
+          else {return res.status(401).send('Invalid password');}
+        }
+      }
+      // if no err or user, respond false to passport strategy to go ahead with user creation
+      else { def.resolve(null); }
+    });
+    return def.promise;
+  } ,
+
   getCurrentUser: function(req, res){
     console.log(req.session, req.user);
     return req.user;
