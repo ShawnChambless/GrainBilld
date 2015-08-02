bbb3cf13f263cab8da11528887a66c158efecf13
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-01 18:04:47 -0600

    remove unnecessary function

--- api/controllers/users/userCtrl.js
+++ api/controllers/users/userCtrl.js
@@ -5,7 +5,6 @@ var mongoose    = require('mongoose') ,
     createHash  = function(password){ return bcrypt.hashSync(password); } ,
     checkHash   = function(password, hash){ return bcrypt.compareSync(password, hash); } ;
 
-// usage of 'q' promises is for auth functionality. see passport.js
 module.exports = {
 
   create: function(req, res){
@@ -23,51 +22,6 @@ module.exports = {
       }
       else {
         if (req.qpromise) {def.resolve(createdUser);}
-        else {return res.status(200).json(createdUser);}
-      }
-    });
-    return def.promise;
-  } ,
-
-  retrieveOne: function(req, res){
-    var def = $q.defer();
-    var query = {};
-    if (req.user || req.params.user_id) query = { "_id": req.params.user_id };
-    else query = { "email": req.body.email };
-    User.findOne(query)
-    .exec().then(function(user, err){
-      if (err) {
-        console.log(err);
-        if (req.qpromise) {def.reject(err);}
-        else {return res.status(500).json(err);}
-      }
-      else if (user) {
-        if (query._id) return res.status(200).json(user);
-        if (checkHash(req.body.password, user.password)) {
-          if (req.qpromise) {def.resolve(user);}
-          else {return res.status(200).json(user);}
-        }
-        if (!checkHash(req.body.password, user.password)) {
-          if (req.qpromise) {def.reject('Invalid password');}
-          else {return res.status(401).send('Invalid password');}
-        }
-      }
-      // if no err or user, respond false to passport strategy to go ahead with user creation
-      else { def.resolve(null); }
-    });
-    return def.promise;
-  } ,
-
-  retrieveAll: function(req, res){
-    var def = $q.defer();
-    User.find({})
-    .exec().then(function(users, err){
-      if (err) {
-        if (req.qpromise) {def.reject(err);}
-        else {return res.status(500).json(err);}
-      }
-      else {
-        if (req.qpromise) {def.resolve(users);}
         else {return res.status(200).json(createdUser);}
       }
     });
@@ -92,18 +46,5 @@ module.exports = {
       return res.status(200).send('User ' + req.params.user_id + ' has been deleted');
     });
   },
-
-  updatePosts: function(req, res){
-      User.findById(req.params.user_id, function(err, user){
-          if(err) return res.status(500).json(err);
-          console.log(111111, user, typeof user.favorites);
-          user.favorites.push(new mongoose.Types.ObjectId(req.params.post_id));
-          user.save(function(error, newUser){
-              console.log(222222, newUser, error);
-              if(error) return res.status(500).json(error);
-              res.json(newUser);
-          });
-      });
-  }
 
 };
